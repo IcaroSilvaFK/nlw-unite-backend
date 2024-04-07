73d3d9bfddda362c0801a3b52878b95d861716bc
whyy <icarovsilva1@gmail.com>
2024-04-06 21:09:52 -0300

    feat: add build script

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,22 @@ import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fast
 import fastifyPrismaPlugin from './lib/prisma'
 import fastifySwagger from "@fastify/swagger"
 import fastifySwaggerUI from '@fastify/swagger-ui'
+import { ZodError } from "zod";
+import fastifyCors from "@fastify/cors";
 
 async function main() {
   const app = fastify()
 
   try {
 
+    await app.register(fastifyCors, {
+      origin: "*"
+    })
+
     app.setValidatorCompiler(validatorCompiler)
     app.setSerializerCompiler(serializerCompiler)
 
-    app.register(fastifySwagger, {
+    await app.register(fastifySwagger, {
       swagger: {
         consumes: ["application/json"],
         produces: ["application/json"],
@@ -25,19 +31,29 @@ async function main() {
       },
       transform: jsonSchemaTransform,
     })
-    app.register(fastifySwaggerUI, {
+    await app.register(fastifySwaggerUI, {
       routePrefix: "/docs"
     })
 
     app.setErrorHandler((error, _, reply) => {
+      const { validationContext } = error
+
+      if (error instanceof ZodError) {
+        return reply.status(400).send({
+          message: `Error on validating request ${validationContext}`,
+          code: 400,
+          causes: error.flatten().fieldErrors,
+        })
+      }
+
       return reply.code(+error.code).send({
         message: error.message,
         code: error.code,
-        cause: error?.cause
+        causes: error?.cause
       })
     })
 
-    app.register(fastifyPrismaPlugin)
+    await app.register(fastifyPrismaPlugin)
 
     await app.register(router)
 
@@ -45,6 +61,7 @@ async function main() {
 
     await app.listen({
       port: 3333,
+      host: '0.0.0.0'
     }).then(res => console.log(`Server running at ${res}`))
   } catch (err) {
     app.log.error(err)
